0d7ae4a7152e9a054db07c742e44dd2c8f7d25b1
David <daringcaleb@gmail.com>
2022-06-06 18:44:34 +0100

    new cloud

--- connection.py
+++ connection.py
@@ -8,8 +8,8 @@ def connect_elasticsearch():
     # _es = None
     try:
         _es = None
-        _es = Elasticsearch(cloud_id="test:ZWFzdHVzMi5henVyZS5lbGFzdGljLWNsb3VkLmNvbTo5MjQzJDk1NjgxMjUzZDJiYzQ4ZTNiNWFjNzZjMDdlNDliZGY4JDI5YWEyOGQxZTA0MDQxYjJhNzcwNGE5NjZkYzQxZWUw",
-                            basic_auth=("elastic", "r3iheQwoQR5JEl4kQ1pEcmv3"), verify_certs=False, request_timeout=100)
+        _es = Elasticsearch(cloud_id="test:dXMtY2VudHJhbDEuZ2NwLmNsb3VkLmVzLmlvJDY2ZTg1MDE4NGQ1ZDQwZTJhYzRlNjJmNTE4MzE5YmNkJGI4YWY4ZjJlYWU4NDRjOGY5MzhhYzE3MzhhY2Q2YWE1",
+                            basic_auth=("elastic", "2ycWZCoHaEESN5xvl7GhHzUP"), verify_certs=False, request_timeout=100)
 
         if _es.info():
             print('Connected')
